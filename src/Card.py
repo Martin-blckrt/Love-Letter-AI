8b6c9d70760fc4b096b65fb61862e3ac87843d18
Martin-blckrt <martinblanckaert@yahoo.fr>
2020-10-19 09:02:08 +0000

    cards changed

--- src/Card.py
+++ src/Card.py
@@ -1,24 +1,18 @@
 class Card:
-    def __init__(self, title, value, totalNumber, leftNumber, description, assets):
+    def __init__(self, title, value, totalNumber, description):
         self.title = title
         self.value = value
         self.totalNumber = totalNumber
-        self.leftNumber = leftNumber
+        self.leftNumber = totalNumber
         self.description = description
-        self.assets = assets
+        # self.assets = assets 
 
     # TODO. Ecrire les methodes de la class Card
 
 
 class Spy(Card):
-    def __init__(self):
-        self.title = "Spy"
-        self.value = 0
-        self.totalNumber = 2
-        self.description = "Gardez un pion Faveur si personne ne joue ou défausse une carte Espionne."
 
-
-Spy_Card = Spy()  # on devrait utiliser les constructeurs dans un fichier init, pas dans le fichier Card
+Spy_Card = Spy("Spy", 0, 2, "Gardez un pion Faveur si personne ne joue ou défausse une carte Espionne.")  # on devrait utiliser les constructeurs dans un fichier init, pas dans le fichier Card
 
 
 # TODO. Creer un fichier initialisation qui contient une fonction appelant tous les constructeurs pour "creer" et
@@ -26,99 +20,52 @@ Spy_Card = Spy()  # on devrait utiliser les constructeurs dans un fichier init, 
 
 
 class Guard(Card):
-    def __init__(self):
-        self.title = "Guard"
-        self.value = 1
-        self.totalNumber = 6
-        self.description = "Devinez la main d'un autre joueur."
 
-
-Guard_Card = Guard()
+Guard_Card = Guard("Guard", 1, 6, "Devinez la main d'un autre joueur.")
 
 
 class Priest(Card):
-    def __init__(self):
-        self.title = "Priest"
-        self.value = 2
-        self.totalNumber = 2
-        self.description = "Regardez la main d'un autre joueur."
 
-
-Priest_Card = Priest()
+Priest_Card = Priest("Priest", 2, 2, "Regardez la main d'un autre jouer.")
 
 
 class Baron(Card):
-    def __init__(self):
-        self.title = "Baron"
-        self.value = 3
-        self.totalNumber = 2
-        self.description = "Comparez votre main avec celle d'un autre joueur."
 
 
-Baron_Card = Baron()
+Baron_Card = Baron("Baron", 3, 2, "Comparez votre main avec celle d'un autre joueur.")
 
 
 class Handmaid(Card):
-    def __init__(self):
-        self.title = "Handmaid"
-        self.value = 4
-        self.totalNumber = 2
-        self.description = "Les autres cartes n'ont pas d'effet sur vous jusqu'au prochain tour."
 
 
-Handmaid_Card = Handmaid()
+Handmaid_Card = Handmaid("Handmaid", 4, 2, "Les autres cartes n'ont pas d'effet sur vous jusqu'au prochain tour.")
 
 
 class Prince(Card):
-    def __init__(self):
-        self.title = "Prince"
-        self.value = 5
-        self.totalNumber = 2
-        self.description = "Défaussez votre main et piochez à nouveau."
 
 
-Prince_Card = Prince()
+Prince_Card = Prince("Prince", 5, 2, "Défaussez votre main et piochez à nouveau.")
 
 
 class Chancellor(Card):
-    def __init__(self):
-        self.title = "Chancellor"
-        self.value = 6
-        self.totalNumber = 2
-        self.description = "Piochez et remettez deux cartes sous le paquet."
 
 
-Chancellor_Card = Chancellor()
+Chancellor_Card = Chancellor("Chancellor", 6, 2, "Piochez et remettez deux cartes sous le paquet.")
 
 
 class King(Card):
-    def __init__(self):
-        self.title = "King"
-        self.value = 7
-        self.totalNumber = 1
-        self.description = "Échangez votre main contre celle d'un autre joueur."
 
 
-King_Card = King()
+King_Card = King("King", 7, 1, "Échangez votre main contre celle d'un autre joueur.")
 
 
 class Countess(Card):
-    def __init__(self):
-        self.title = "Countess"
-        self.value = 8
-        self.totalNumber = 1
-        self.description = "Vous devez impérativement la jouer si vous avez le Roi ou un Prince."
 
 
-Countess_Card = Countess()
+Countess_Card = Countess("Countess", 8, 1, "Vous devez impérativement la jouer si vous avez le Roi ou un Prince.")
 
 
 class Princess(Card):
-    def __init__(self):
-        self.title = "Princess"
-        self.value = 9
-        self.totalNumber = 1
-        self.description = "Quittez le tour si vous devez la jouer."
 
 
-Princess_Card = Princess()
+Princess_Card = Princess("Princess", 9, 1, "Quittez la manche si vous devez la jouer.")
