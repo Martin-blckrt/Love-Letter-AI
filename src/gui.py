b0fb1b79499d664dcc135567eee559f6e5490ee9
DefrostedWaffle <sirventhomas@gmail.com>
2020-10-22 21:42:16 +0200

    GUI Implementation test Commit made to make it available to all authors

---
+++ src/gui.py
@@ -0,0 +1,42 @@
+"""
+GUI Module with PySimpleGUI
+"""
+
+import PySimpleGUI as sg
+
+
+def createButton(button_text):
+    """
+    Fonction qui automatise la création de boutton
+
+    :param button_text: sera affiché sur le bouton,
+    et sera aussi le nom de l'event associé (Ex: 'Quitter')
+    :return: un élément Button utilisable et initialisé
+    """
+    return sg.Button(button_text, button_color=('white', 'grey'), size=(6, 2),
+                     pad=(5, 5), font=('Arial', 20))
+
+
+def initializeWindow():
+    """
+    Initialize the game window and all the elements
+    """
+    sg.theme('DarkAmber')   # Add a touch of color
+
+    # All the stuff inside your window.
+    layout = [[sg.Text('Love Letter', font='Arial 42')],
+              [sg.Image(r'C:\Users\thoma\Documents\Coding\PyCharm\LoveLetter\src\assets\title_image.png')],
+              [createButton('Ok'), createButton('Quitter')]]
+
+    # Create the Window
+    window = sg.Window('Love Letter', layout, size=(840, 500), keep_on_top=True, finalize=True)
+    window.Maximize()
+
+    # Event Loop to process "events" and get the "values" of the inputs
+    while True:
+        event, values = window.read()
+
+        if sg.WIN_CLOSED or 'Quitter' in event:  # if user closes window or clicks cancel
+            break
+
+    window.close()
