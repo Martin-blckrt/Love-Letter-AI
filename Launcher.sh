2a80c5fcf0b4f33a0dd66fd2dd325b9fa2796cb1
Alexandre <alexandre.des3os@gmail.com>
2020-12-22 18:40:48 +0100

    change bash file

--- Launcher.sh
+++ Launcher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! [ -x "$(command -v python3)" ]; then
-  echo 'Error: python3 is not installed.' >&2
+  echo 'Error: python3 might not be installed.' >&2
   exit 1
 else
   python3 -u main.py
